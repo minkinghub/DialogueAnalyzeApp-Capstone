f149f328b61a565696433767ded5c1af6bff162b
minkinghub <kp141@naver.com>
2024-05-02 16:48:01 +0900

    5.2 - project Merge

--- front/index.js
+++ front/index.js
@@ -4,7 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src';
 import {name as appName} from './app.json';
 import Home from './mainPage/Home';
 import Analyze from './analyzePage/Analyze';
