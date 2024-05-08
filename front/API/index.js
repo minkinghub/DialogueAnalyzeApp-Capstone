332596deba67edc7cff70b7460f19ac2cc12a6c8
apiod <trtxcrop4893@naver.com>
2024-05-08 16:19:04 +0900

    1st commit

--- front/API/index.js
+++ front/API/index.js
@@ -1,41 +1,52 @@
+import {useEffect} from 'react';
 import {axiosAuthApi} from './instance';
+const API = () => {
+  const [historyKey, sethistoryKey] = useState('');
 
-const getDetail = async historyKey => {
-  try {
-    const response = await axiosAuthApi.post(
-      'user/history/detail',
-      {historyKey: historyKey},
-      {
-        headers: {
-          'Content-Type': 'application/json',
+  useEffect(() => {
+    const fetchData = async () => {};
+    fetchData();
+  }, []);
+
+  const getDetail = async historyKey => {
+    try {
+      const response = await axiosAuthApi.post(
+        'user/history/detail',
+        {historyKey: historyKey},
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
         },
+      );
+      console.log('response.data:', response.data);
+
+      return response.data;
+    } catch (error) {
+      console.error('Error getDetail data:', error);
+    }
+    if (historyKey) {
+    }
+  };
+
+  const getHistory = async () => {
+    const response = await axiosAuthApi.post('user/history', {
+      headers: {
+        'Content-Type': 'application/json',
       },
-    );
+    });
     console.log('response.data:', response.data);
-
     return response.data;
-  } catch (error) {
-    console.error('Error getDetail data:', error);
-  }
-};
+  };
 
-const getHistory = async () => {
-  const response = await axiosAuthApi.post('user/history', {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  console.log('response.data:', response.data);
-  return response.data;
+  const uploadFile = async formData => {
+    const response = await axiosAuthApi.post('upload/analyze/text', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    sethistoryKey(response.data.historyKey);
+    return response;
+  };
 };
-
-const uploadFile = async formData => {
-  const response = await axiosAuthApi.post('upload/analyze/text', formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  });
-  return response;
-};
-
 export {getDetail, getHistory, uploadFile};
