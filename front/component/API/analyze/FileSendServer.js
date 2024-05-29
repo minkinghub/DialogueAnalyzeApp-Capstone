cd3f5b05dce6d7bdefa3c789f0263b595730332e
HyoShin <sinto825@naver.com>
2024-05-29 19:22:24 +0900

    5.29 분석 통합

--- front/component/API/analyze/FileSendServer.js
+++ front/component/API/analyze/FileSendServer.js
@@ -3,7 +3,7 @@ import { View, Text, ActivityIndicator} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {uploadFile} from '../../../API';
 
-const FileSendServer = ({ selectedFile, opAge_range, analysisType, onCompleted, resetFile}) => {
+const FileSendServer = ({ selectedFile, onCompleted, resetFile}) => {
     const navigation = useNavigation(); //네비게이션 객체
     const [isLoading, setIsLoading] = useState(false); // 로딩 상태
 
@@ -12,8 +12,6 @@ const FileSendServer = ({ selectedFile, opAge_range, analysisType, onCompleted, 
           // formData 객체 생성
           const formData = new FormData();
           formData.append('file', selectedFile);
-          formData.append('opAge_range', opAge_range);
-          formData.append('analysisType', analysisType);
 
           try {
             setIsLoading(true); // 로딩 시작
@@ -21,13 +19,16 @@ const FileSendServer = ({ selectedFile, opAge_range, analysisType, onCompleted, 
             const res = await uploadFile(formData);
             console.log('File upload server response:', res.data);
 
-            if (analysisType === true) {
-              navigation.navigate('Etiquette', { historyKey: res.data.historyKey });
-              resetFile();
-            } else {
-              navigation.navigate('Category', { historyKey: res.data.historyKey });
-              resetFile();
-            }
+            navigation.navigate('Etiquette', { historyKey: res.data.historyKey });
+            resetFile();
+            
+            // if (analysisType === true) {
+            //   navigation.navigate('Etiquette', { historyKey: res.data.historyKey });
+            //   resetFile();
+            // } else {
+            //   navigation.navigate('Category', { historyKey: res.data.historyKey });
+            //   resetFile();
+            // }
           } catch (error) {
               if (error.response) {
                 console.error('Error status:', error.response.status);
@@ -55,8 +56,8 @@ const FileSendServer = ({ selectedFile, opAge_range, analysisType, onCompleted, 
         bottom: 0,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)', // 배경을 반투명하게 설정
-        zIndex: 9999, // 다른 컴포넌트 위에 오도록 설정
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+        zIndex: 9999, 
       }}>
           {isLoading && (
             <View style={{justifyContent: 'center', alignItems: 'center',}}>
--- front/component/analyze/FileChoice.js
+++ front/component/analyze/FileChoice.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { TouchableOpacity, Text, Alert, Image } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { TouchableOpacity, Text, Alert, ScrollView } from 'react-native';
 import DocumentPicker from 'react-native-document-picker';
 import {useContext} from 'react';
 import ThemeContext from '../../src/ThemeContext';
@@ -10,22 +10,29 @@ const FileChoice = ( {onFileSelected} ) => {
     const DarkMode = useContext(ThemeContext);
     const isDarkMode = DarkMode.isDarkMode;
     const theme = isDarkMode ? darkTheme : lightTheme;
+    const [selectedFile, setSelectedFile] = useState('');
 
     const fileChoice = async () => {
         try {
             const res = await DocumentPicker.pick({
                 type: [DocumentPicker.types.allFiles],
             });
-            onFileSelected(res);
+            setSelectedFile(res);
 
             //파일 검사
             const isTxtFile = res[0].type === 'text/plain' || res[0].name.endsWith('.txt');
+            const isAudioFile = res[0].type.startsWith('audio/') || res[0].name.endsWith('.mp3') || res[0].name.endsWith('.wav');
 
             if (isTxtFile) {
-                //파일이 선택 되었을 경우 반환
+                // 텍스트 파일이 선택 되었을 경우
+                res[1] = true; //dataType = true
+                onFileSelected(res);
+            } else if (isAudioFile) {
+                // 음성 파일이 선택 되었을 경우
+                res[1] = false; //dataType = false
                 onFileSelected(res);
             } else {
-                Alert.alert("안내", "텍스트 파일만 선택해주세요.");
+                Alert.alert("안내", "텍스트 또는 음성 파일만 선택해주세요.");
             }
         } catch (err) {
             if (DocumentPicker.isCancel(err)) {
@@ -40,19 +47,29 @@ const FileChoice = ( {onFileSelected} ) => {
     return (
         <TouchableOpacity
             style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: theme.backgroundColor,
-                width: '80%',
-                height: '90%',
-                borderRadius: 15,
-                borderWidth: 1.5,
-                borderColor: theme.borderColor
-            }}
-            onPress={fileChoice}
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '10%',
+            width: '90%',
+            marginTop: 10,
+            borderRadius: 10,
+            borderWidth: 1,
+            borderStyle: 'dashed',
+            borderColor: theme.borderColor,
+            padding: 10,
+            }} onPress={fileChoice}
         >
-            <Text style={{color: theme.textColor, fontSize: 15}}>터치 하여</Text>
-            <Text style={{color: theme.textColor, fontSize: 15}}>텍스트 파일 선택</Text>
+            <ScrollView
+                horizontal
+                contentContainerStyle={{ alignItems: 'center' }}
+                showsHorizontalScrollIndicator={true}
+            >
+                <Text style={{color: theme.textColor, fontSize: 17}}>
+                {selectedFile
+                    ? '선택한 파일 : ' + selectedFile[0].name
+                    : '선택한 파일 없음'}
+                </Text>
+            </ScrollView>
         </TouchableOpacity>
     );
 };
