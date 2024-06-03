4259d55b7535dff85e72687d8d47c401c9538ed6
apiod <trtxcrop4893@naver.com>
2024-06-03 13:51:39 +0900

    fix

--- front/src/analyze/etiquette.jsx
+++ front/src/analyze/etiquette.jsx
@@ -1,20 +1,28 @@
-//보고있는 기준 테두리 표시
-//총점수 표시
-import React, {useRef, useState, useEffect} from 'react';
-import {View, Text, TouchableOpacity, ScrollView} from 'react-native';
+import React, {useRef, useState, useEffect, useCallback} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
 import {useTheme} from '../ThemeContext';
 import {loadDatail} from './loadData';
 import useSpeakerPicker from './speakerPicker';
 import ActivityIndicatorLoading from './ActivityIndicatorLoading';
 import etiquetteStyles from './stylesFile/etiquette.style';
+import {labelKr, commentType} from './utilities';
+import {DrawBarChart} from './DrawChart';
+const screenWidth = Dimensions.get('window').width;
 
 const Etiquette = ({route}) => {
   const scrollViewRef = useRef(null);
   const historyKey = route.params.historyKey;
-  const [CommentHeight, setCommentHeight] = useState([]); //ScrollToItem을 위한 변수, 값을 계산하기 위해 사용, 동적인 높이를 위해 사용
+  const [CommentHeight, setCommentHeight] = useState([]); // ScrollToItem을 위한 변수, 값을 계산하기 위해 사용, 동적인 높이를 위해 사용
   const [detailList, setDetailList] = useState(undefined);
-  const [speaker, setSpeaker] = useState([]); //speaker 목록을 저장하기 위한 변수, Picker에 사용
+  const [speaker, setSpeaker] = useState([]); // speaker 목록을 저장하기 위한 변수, Picker에 사용
   const {selpeaker, renderSpeakerPicker} = useSpeakerPicker(speaker);
+  const [currentLabelIndex, setCurrentLabelIndex] = useState(0); // 현재 보고 있는 항목의 인덱스 저장
 
   const {isDarkMode} = useTheme();
   const styles = etiquetteStyles(isDarkMode);
@@ -22,11 +30,11 @@ const Etiquette = ({route}) => {
   useEffect(() => {
     const fetchData = async () => {
       const data = await loadDatail(historyKey);
-      console.log('data.detailList:', data.detailList);
+      // console.log('data.detailList:', data.detailList);
       setDetailList(data.detailList);
       setSpeakerList(data.detailList);
     };
-    //새로 함수를 안만들고 진행시 비동기 문제 발생 해결하기 위해 data가 들어오면 실행시키기 위함
+    // 새로 함수를 안만들고 진행시 비동기 문제 발생 해결하기 위해 data가 들어오면 실행시키기 위함
     const setSpeakerList = data => {
       const speakerList = [];
       data.map(item => {
@@ -37,47 +45,70 @@ const Etiquette = ({route}) => {
     };
     fetchData();
   }, [historyKey]);
-  const labelKr = item => {
-    if (item === 'polite') {
-      return '존댓말';
-    } else if (item === 'moral') {
-      return '불쾌발언';
-    } else if (item === 'grammar') {
-      return '맞춤법';
-    } else if (item === 'positive') {
-      return '감정';
-    }
-  };
+
   const DrawTable = ({scrollViewRef}) => {
-    //해당 standard의 comment를 이동하기 위한 함수
+    // 해당 standard의 comment를 이동하기 위한 함수
     const scrollToItem = index => {
       // scrollTo 위치 계산
       let nowHeight = 0;
       for (let i = 0; i < index; i++) {
-        nowHeight += CommentHeight[i];
+        nowHeight += CommentHeight[i] || 0;
       }
       scrollViewRef.current.scrollTo({
         y: nowHeight,
         animated: true,
       });
     };
+
     return detailList ? (
       <View style={styles.tableView}>
         <Text style={styles.tableComment}>기준 클릭시 상세 내용으로 이동</Text>
+        <View style={styles.tableTitleView}>
+          <Text style={styles.tableTitle}>사용 빈도 </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              width: '70%',
+            }}>
+            <Text style={styles.tableTitle}>부정</Text>
+            <Text style={styles.tableTitle}>긍정</Text>
+          </View>
+        </View>
         {detailList[selpeaker]?.detailInfo.map((item, index) => (
           <TouchableOpacity
             key={index}
-            style={styles.labelView}
-            //해당 standard의 comment로 이동
+            style={[
+              styles.labelView,
+              currentLabelIndex === index && styles.HighlightStyle,
+            ]}
+            // 해당 standard의 comment로 이동
             onPress={() => {
               scrollToItem(index);
             }}>
             <View style={styles.labelTextView}>
-              <Text style={styles.labelText}>{labelKr(item.label)}</Text>
+              <Text
+                style={[
+                  styles.labelText,
+                  currentLabelIndex === index && styles.HighlightTextStyle,
+                ]}>
+                {labelKr(item.label)}
+              </Text>
             </View>
             <View style={styles.lableScoreTextView}>
+              <Text style={styles.lableScoreText}>{item.detailScore}</Text>
+              <View style={styles.scoreBar}>
+                <View
+                  style={[
+                    styles.scoreBarWaring,
+                    {
+                      width: item.detailScore + '%',
+                    },
+                  ]}
+                />
+              </View>
               <Text style={styles.lableScoreText}>
-                {item.detailScore} 점입니다.
+                {100 - item.detailScore}
               </Text>
             </View>
           </TouchableOpacity>
@@ -85,75 +116,118 @@ const Etiquette = ({route}) => {
       </View>
     ) : null;
   };
-  //Standard Comment의 세로 길이를 계산하기 위한 함수
 
+  // Standard Comment의 세로 길이를 계산하기 위한 함수
   const handleLayout = (index, event) => {
     const height = event.nativeEvent.layout.height;
-    // console.log('height:', height);
-    setCommentHeight([...CommentHeight, height]);
+    // height 값을 업데이트하기 위해 이전 높이 배열을 복사하여 수정합니다.
+    setCommentHeight(prevHeights => {
+      const newHeights = [...prevHeights];
+      newHeights[index] = height;
+      return newHeights;
+    });
   };
+
+  // 스크롤 이벤트 핸들러
+  const handleScroll = event => {
+    const yOffset = event.nativeEvent.contentOffset.y;
+    let nowHeight = 0;
+    for (let i = 0; i < CommentHeight.length; i++) {
+      nowHeight += CommentHeight[i] || 0;
+      if (yOffset < nowHeight) {
+        setCurrentLabelIndex(i);
+        break;
+      }
+    }
+  };
+
   return detailList ? (
     <View key="3000" style={styles.container}>
       <View key="3100" style={styles.headerStyle}>
         <Text style={styles.headerTextStyle}>예절 분석 결과</Text>
         <View key="3110" style={{width: '32%'}}>
-          {/* <SpeakerPicker /> */}
           {renderSpeakerPicker()}
         </View>
       </View>
 
       <DrawTable scrollViewRef={scrollViewRef} />
 
-      <ScrollView key="3300" ref={scrollViewRef} style={styles.scrollView}>
-        {detailList[selpeaker].detailInfo.map((item, labelIndex) => {
-          // console.log(item.label, ': ', item);
-          const infoKey = item.label + labelIndex.toString();
-          return (
-            <View
-              key={infoKey}
-              style={styles.commentView}
-              onLayout={event => handleLayout(labelIndex, event)}>
-              <View style={styles.commentLabelTextView}>
-                <Text style={styles.commentLabelText}>
-                  {labelKr(item.label)}
-                </Text>
-              </View>
-              <View style={styles.commentExView}>
-                {item.exampleText.map((item, index) => {
-                  const key = infoKey + index.toString();
-                  // console.log('item:', item);
-                  return (
-                    <View key={key} style={{margin: 5}}>
-                      <View style={styles.commentExTextView}>
-                        <View style={styles.commentTitleTextView}>
-                          <Text
-                            style={[styles.commentTitleText, {fontSize: 16}]}>
-                            대화내용
-                          </Text>
+      <View style={styles.scrollViewContainer}>
+        <ScrollView
+          key="3300"
+          ref={scrollViewRef}
+          onScroll={handleScroll}
+          scrollEventThrottle={16}>
+          {detailList[selpeaker].detailInfo.map((infoItem, labelIndex) => {
+            const infoKey = infoItem.label + labelIndex.toString();
+            return (
+              <View
+                key={infoKey}
+                style={styles.commentView}
+                onLayout={event => handleLayout(labelIndex, event)}>
+                <View style={styles.commentLabelTextView}>
+                  <Text style={styles.commentLabelText}>
+                    {labelKr(infoItem.label)}
+                  </Text>
+                </View>
+                <View style={styles.commentExView}>
+                  {infoItem.exampleText ? (
+                    infoItem.exampleText.map((item, index) => {
+                      const key = infoKey + index.toString();
+                      return (
+                        <View key={key} style={{margin: 5}}>
+                          <View style={styles.commentExTextView}>
+                            <View style={styles.commentTitleTextView}>
+                              <Text
+                                style={[
+                                  styles.commentTitleText,
+                                  {fontSize: 16},
+                                ]}>
+                                대화내용
+                              </Text>
+                            </View>
+                            <Text style={styles.commentExText}>
+                              {item.chatContent}
+                            </Text>
+                          </View>
+                          {
+                            // 불쾌한 발언, 감정 표현일 경우에만 표시
+                            infoItem.label === 'moral' ||
+                            infoItem.label === 'positive' ? (
+                              <View style={styles.commentExTextView}>
+                                <View style={styles.commentTitleTextView}>
+                                  <Text
+                                    style={[
+                                      styles.commentTitleText,
+                                      {fontSize: 12},
+                                    ]}>
+                                    감지된 표현
+                                  </Text>
+                                </View>
+                                <Text style={styles.commentExText}>
+                                  {commentType(infoItem.label, item.isStandard)}
+                                  이 감지되었습니다.
+                                </Text>
+                              </View>
+                            ) : null
+                          }
                         </View>
-                        <Text style={styles.commentExText}>
-                          {item.chatContent}
-                        </Text>
-                      </View>
-                      <View style={styles.commentExTextView}>
-                        <View style={styles.commentTitleTextView}>
-                          <Text
-                            style={[styles.commentTitleText, {fontSize: 12}]}>
-                            감지된 표현
-                          </Text>
-                        </View>
-                        <Text style={styles.commentExText}>
-                          {item.isStandard}
-                        </Text>
-                      </View>
+                      );
+                    })
+                  ) : (
+                    //만점을 맞았을 경우 없음을 표시
+                    <View style={styles.anyThingTextView}>
+                      <Text style={styles.anyThingText}>
+                        감지된 표현이 없습니다.
+                      </Text>
                     </View>
-                  );
-                })}
+                  )}
+                </View>
               </View>
-            </View>
-          );
-        })}
-      </ScrollView>
+            );
+          })}
+        </ScrollView>
+      </View>
     </View>
   ) : (
     <ActivityIndicatorLoading />
